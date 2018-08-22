6d1d82767761f4b7a10dcdbbd5a8cc9e4e993355
KindTis <tatis3@gmail.com>
2018-08-22 17:48:34 +0900

    - 첫 캡쳐 테스트

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Drawing;
+using ShareX.ScreenCaptureLib;
+
+namespace ImageCompare
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ScreenCapture(object sender, RoutedEventArgs e)
+        {
+            Rectangle rect = new Rectangle(100, 100, 640, 480);
+            Screenshot screen = new Screenshot();
+            Image img = screen.CaptureRectangle(rect);
+            img.Save("c:\\some.jpg");
+        }
+    }
+}
