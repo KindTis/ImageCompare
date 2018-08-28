072387456346f42b17cc1172eadb47634496ddcb
KindTis <tatis3@gmail.com>
2018-08-28 17:45:49 +0900

    - 이미지 비교 모듈 추가 - 캡쳐 이미지를 하위 폴더에 넘버링을 이용하여 자동 저장 - 캡쳐 리스트를 ListBox에 추가하여 관리할 수 있도록 작업 중

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Collections.ObjectModel;
 
 namespace ImageCompare
 {
@@ -13,5 +14,21 @@ namespace ImageCompare
     /// </summary>
     public partial class App : Application
     {
+    }
+
+    public class MockMasterViewModel
+    {
+
+        public ObservableCollection<CompareListItem> Items { get; set; }
+
+        public MockMasterViewModel()
+        {
+            var item01 = new CompareListItem() { ImagePath = "d:\\Projects\\ImageCompare\\src.jpg", Region = "{ x: 100, y: 200, width: 500, height: 500 }", Key = "Key" };
+            var item02 = new CompareListItem() { ImagePath = "d:\\Projects\\ImageCompare\\dst.jpg", Region = "{ x: 0, y: 0, width: 300, height: 250 }", Key = "Key" };
+            Items = new ObservableCollection<CompareListItem>()
+            {
+                item01, item02
+            };
+        }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -4,28 +4,63 @@ using System.Windows.Forms;
 using System.Collections.Generic;
 using System.Drawing;
 using ShareX.ScreenCaptureLib;
+using XnaFan.ImageComparison;
+using System.IO;
 
 namespace ImageCompare
 {
-    /// <summary>
-    /// MainWindow.xaml에 대한 상호 작용 논리
-    /// </summary>
+    public class CompareListItem
+    {
+        public string ImagePath { get; set; }
+        public string Region { get; set; }
+        public string Key { get; set; }
+    }
+
     public partial class MainWindow : Window
     {
+        public Rectangle CaptureRegion = new Rectangle();
+        private Screenshot mScreenShot = new Screenshot();
+        private List<CompareListItem> mCompareListItems = new List<CompareListItem>();
+        private string mSubDir = "Images";
+        private int mCaptureCount = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+            CompareList.ItemsSource = mCompareListItems;
+            Directory.CreateDirectory(mSubDir);
         }
 
         private void ScreenCapture(object sender, RoutedEventArgs e)
         {
-            Rectangle rect = new Rectangle();
             RegionCaptureOptions option = new RegionCaptureOptions();
-            RegionCaptureTasks.GetRectangleRegion(out rect, option);
-            Image1.Text = rect.ToString();
-            Screenshot screen = new Screenshot();
-            Image copyImg = screen.CaptureRectangle(rect);
-            copyImg.Save("c:\\some.jpg");
+            RegionCaptureTasks.GetRectangleRegion(out CaptureRegion, option);
+            Image copyImg = mScreenShot.CaptureRectangle(CaptureRegion);
+            if (copyImg != null)
+            {
+                string savePath = mSubDir + "\\Capture" + mCaptureCount.ToString("d3") + ".jpg";
+                mCaptureCount++;
+                copyImg.Save(savePath);
+                mCompareListItems.Add(new CompareListItem()
+                {
+                    ImagePath = Path.GetFullPath(savePath),
+                    Region = CaptureRegion.ToString(),
+                    Key = ""
+                });
+                CompareList.Items.Refresh();
+            }
+        }
+
+        private void DuplicateCapture(object sender, RoutedEventArgs e)
+        {
+            Image copyImg = mScreenShot.CaptureRectangle(CaptureRegion);
+            copyImg.Save("c:\\dst.jpg");
+        }
+
+        private void CompareImage(object sender, RoutedEventArgs e)
+        {
+            int difference = (int)(ImageTool.GetPercentageDifference("c:\\src.jpg",
+                "c:\\dst.jpg") * 100);
         }
     }
 }
